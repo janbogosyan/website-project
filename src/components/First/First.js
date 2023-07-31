2cac708c9eb63b270ea021bc9500b14d72888db5
Jan Bogosyan <janbogosyan@gmail.com>
2023-08-01 00:08:44 +0300

    project

---
+++ src/components/First/First.js
@@ -0,0 +1,70 @@
+import Header from '../Header/Header';
+import classes from './First.module.css';
+import Goku from '../../assets/goku-png-23549.png'
+
+const First = () => {
+    return (
+
+        <div className={classes.first}>
+            <div className={classes['left-side']}>
+                <Header />
+
+                {/* the best anime shop */}
+                <div className={classes['the-best-ad']}>
+                    <div></div>
+                    <span>the best anime shop</span>
+                </div>
+                {/*Buy heading */}
+                <div className={classes['size-adjustment']}>
+                    <div className={classes['buy-heading']}>
+                        <div>
+                            {/* this className 'stroke-text' coming from App.css(so no need to import its global) */}
+                            <span className='stroke-text'>Buy</span>
+                            <span>Your</span>
+                        </div>
+                        <div>
+                            <span>Dream items</span>
+                        </div>
+                        <div>
+                            <span>asdasasdkkadskasdsdasdasdasdasdasdasdasdasdasdasd</span>
+                        </div>
+                    </div>
+                    {/* figures */}
+                    <div className={classes['figures']}>
+                        <div>
+                            <span>+1000</span>
+                            <span>ANIME STICKERS</span>
+                        </div>
+                        <div>
+                            <span>+100</span>
+                            <span>ANIME FIGURES</span>
+                        </div>
+                        <div>
+                            <span>+20</span>
+                            <span>VIDEO GAMES</span>
+                        </div>
+                    </div>
+                </div>
+
+                {/* cool buttons */}
+                <div className={classes['cool-buttons']}>
+                    <div>
+                        <buttons className='btn'>Cool Button 1</buttons>
+                    </div>
+                    <div>
+                        <buttons className='btn'>Cool Button 2</buttons>
+                    </div>
+                </div>
+
+            </div>
+            <div className={classes['right-side']}>
+                <div>
+                    <img src={Goku} alt='young goku' />
+                </div>
+                <button className='btn'>COME NOW</button>
+            </div>
+        </div>
+    )
+}
+
+export default First; 
---
+++ src/components/Header/Header.js
@@ -0,0 +1,20 @@
+import classes from './Header.module.css';
+import Logo from '../../assets/NicePng_anime-hair-png_4764979.png'
+const Header = () => {
+
+    return (
+        <div className={classes.header}>
+
+            <img className={classes.logo} src={Logo} alt='logo' />
+
+            <ul className={classes['header-menu']}>
+                <li>Home</li>
+                <li>Shop</li>
+                <li>Why us</li>
+                <li>Discounts</li>
+                <li>Contacts</li>
+            </ul>
+        </div>
+    )
+}
+export default Header;
