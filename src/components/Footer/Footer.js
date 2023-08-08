4eb528b65bfa98ce58624e303f3fd901cad352d1
Jan Bogosyan <janbogosyan@gmail.com>
2023-08-08 18:22:02 +0300

    new css changes

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -7,8 +7,10 @@ const Footer = () => {
         <div className={classes['footer-container']}>
             <hr />
             <div className={classes.footer}>
+                <div className={classes['social-links']}>
                 <img src={Instagram} alt='instagram logo' />
                 <img src={Phone} alt='phone logo' />
+                </div>
             </div>
         </div>
     )
