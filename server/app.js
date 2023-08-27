9e60e59d18219a8a5aa0f69f5f528369f52e4779
Jan Bogosyan <janbogosyan@gmail.com>
2023-08-27 20:51:07 +0300

    express path for error

--- server/app.js
+++ server/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path')
 const bodyParser = require('body-parser'); //ne e nujno da go instalirame otdelno chrez npm install, vkluchen e kum express version 4.16 i po novi paketni versii
 
 const app = express(); //imeto e po izbor 
@@ -8,13 +9,17 @@ const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({ extended: false }));  //app.use(kogato imame app.use tova znachi che e za celiq app) - using body-parser for the whope app
 
-app.use('/admin',adminRoutes);
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
+// app.use((req, res, next) => {                //za stranici koito ne otgovarqt na nikoi url path setvame error message
+//     res.status(404).send('<h1>Page not found</h1>')
+// })
+
 app.use((req, res, next) => {                //za stranici koito ne otgovarqt na nikoi url path setvame error message
-    res.status(404).send('<h1>Page not found</h1>')
+    res.status(404).sendFile(path.join(__dirname, 'views', 'error-page.html'))
 })
 
-app.listen(3000); 
+app.listen(3000);
 
 
