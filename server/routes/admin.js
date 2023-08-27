a8e7b0826a0565decce6f9983fc741cc8c8b5bb4
Jan Bogosyan <janbogosyan@gmail.com>
2023-08-27 22:13:43 +0300

    util path.js helper function for navigation

--- server/routes/admin.js
+++ server/routes/admin.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const path = require('path')
-
+ const rootDir = require('../util/path')
 
 const router = express.Router();
 
 //   /admin/add-product => GET      zashtoto v app.js sme nastroili taka da e putq  app.use('/admin',adminRoutes);
 router.get('/add-product', (req, res, next) => {
-    res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html')) //  /admin/add-product
+    // res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html')) //  /admin/add-product
+    res.sendFile(path.join(rootDir, 'views', 'add-product.html')) //  /admin/add-product
+
 })
 
 //   /admin/add-product => POST
--- server/routes/shop.js
+++ server/routes/shop.js
@@ -1,11 +1,14 @@
-
 const express = require('express')
 const path = require('path')
+const rootDir = require('../util/path')
+
 
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-    res.sendFile(path.join(__dirname, '../', 'views', 'shop.html')) //s dve dolni cherti __dirname - will point to the path(folder)we are in, in this case routes folder
+    // res.sendFile(path.join(__dirname, '../', 'views', 'shop.html')) //s dve dolni cherti __dirname - will point to the path(folder)we are in, in this case routes folder
+    res.sendFile(path.join(rootDir, 'views', 'shop.html')) //s dve dolni cherti __dirname - will point to the path(folder)we are in, in this case routes folder
+
 })
 
 module.exports = router;
