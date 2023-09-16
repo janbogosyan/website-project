68b6cd1567b9a517b59d7aa60ae3eea7ee2f4b1d
Jan Bogosyan <janbogosyan@gmail.com>
2023-09-16 16:03:22 +0300

    adding cart navigation and other things

--- server/controllers/products.js
+++ server/controllers/products.js
@@ -7,7 +7,7 @@
 const Product = require('../models/product');  //we get it from the model folder , and class must be with Main latter 
 
 exports.getAddProduct = (req, res, next) => {     //exports - with this syntax we can have multiple exports in one file easily/ and exports.getAddProduct(the name .getAddProduct its up to u)
-    res.render('add-product', {
+    res.render('admin/add-product', {
         addProductTitle: 'Add product',
         path: '/admin/add-product',
         formsCSS: true,
@@ -29,7 +29,7 @@ exports.postAddProduct = (req, res, next) => {          //създаваме п�
 
 exports.getProducts = (req, res, next) => { 
     Product.fetchAll( products => {   //inside Product.fetchAll we have anonymous function product => ... //here we call fetchAll from /models.products.js and need to pass a arrow function so when we get our products//така ще ни даде всички продукти , метода fetchAll() сме го създали ние в нашия class Product в models/product.js
-        res.render('shop', {                   //products will be our (cb) argument/parameter we create in /models/products.js  ->  static fetchAll(cb)
+        res.render('shop/product-list', {                   //products will be our (cb) argument/parameter we create in /models/products.js  ->  static fetchAll(cb)
             addProductTitle: 'GOKU Shop',
             prods: products,
             path: '/',
